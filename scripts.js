8728a130126dfed513cd2c023ff2eb5f729ab5fc
Axe2211 <47067718+Axe2211@users.noreply.github.com>
2020-12-29 14:10:17 -0800

    script file for memory card game
    javscript file with card flipping logic

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function setCardEventListeners(){
+
+	const cards = document.querySelectorAll('.memory-card');
+
+	function flipCard(){
+		console.log('I was clicked');
+		console.log(this);
+	}
+
+	cards.forEach(card => card.addEventListener('click', flipCard));
+}
+
+document.addEventListener('DOMContentLoaded', setCardEventListeners);
